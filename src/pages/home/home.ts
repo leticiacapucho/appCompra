84c49815ff3654fcd24e56a182bc64e87b94f905
leticiacapucholuiz <leticiacapucholuiz@gmail.com>
2018-05-29 11:08:24 -0300

    third commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -57,8 +57,45 @@ export class HomePage {
   constructor(
     public navCtrl: NavController, 
     public cursoProvider: CursosProvider) {
-      this.lista = this.cursoProvider.all();
-      //this.cursoProvider.all(); =  quer dizer que chega do meu serviço provider
+     
+      // this.lista = this.cursoProvider.all();
+      //this.cursoProvider.all(); =  quer dizer que chega do meu 
+      
+     // Para executar o serviço. Executar o método. Executar um observador para o webservice, assim ele socilita requisição ao servidor
+    
+   /*   this.cursoProvider.allTeste().subscribe(
+        //tratando erro se o servidor estiver fora do ar, sem sucesso ou com sucesso.
+        
+        //Case se sucesso  
+          sucesso =>{
+              // pegar a lista e preencher a lista com o retorno recebido na response, no caso defini como variável de sucesso
+              this.lista = sucesso;
+          },
+          // tratando o erro 
+          erro => {
+            console.log("Erro: " + erro.message);
+          }
+      ); */
+  }
+
+  // Para atualizar a página / lista
+  ionViewDidEnter() {
+    
+     // Para executar o serviço. Executar o método. Executar um observador para o webservice, assim ele socilita requisição ao servidor
+    
+     this.cursoProvider.allTeste().subscribe(
+      //tratando erro se o servidor estiver fora do ar, sem sucesso ou com sucesso.
+      
+      //Case se sucesso  
+        sucesso =>{
+            // pegar a lista e preencher a lista com o retorno recebido na response, no caso defini como variável de sucesso
+            this.lista = sucesso;
+        },
+        // tratando o erro 
+        erro => {
+          console.log("Erro: " + erro.message);
+        }
+    );
   }
 
   abreDetalhe(item) {
--- src/providers/cursos/cursos.ts
+++ src/providers/cursos/cursos.ts
@@ -10,41 +10,48 @@ export class CursosProvider {
   constructor(public http: HttpClient) {
     console.log('Hello CursosProvider Provider');
   }
-  all(){
+
+  // Criando requisições http
+  allTeste() {
+    // retornando o que tem em cursos. O http transforma para um objeto json
+    return this.http.get<ICurso[]>('http://localhost:3000/cursos');
+  }
+
+  all() {
     const lista: ICurso[] = [
       {
-        id: 1, 
-        titulo: "Curso de ionic", 
-        descricao:"Aprenda ionic na prática", 
-        valor:23.90, 
-        valor_txt:"23,90", 
-        imagem:"https://cdn-images-1.medium.com/max/2000/1*MbVgphFn5dsipV-Sb3eUpg.png",
-        aulas: [
+        "id": 1, 
+        "titulo": "Curso de ionic", 
+        "descricao": "Aprenda ionic na prática", 
+        "valor": 23.90, 
+        "valor_txt": "23,90", 
+        "imagem": "https://cdn-images-1.medium.com/max/2000/1*MbVgphFn5dsipV-Sb3eUpg.png",
+        "aulas": [
           {
-            id: 1,
-            ordem: 1,
-            titulo: "introdução ao Curso",
-            tempo: "10:00",
-            video: "https://www.youtube.com/embed/9XWhNHvGhHU",
+            "id": 1,
+            "ordem": 1,
+            "titulo": "introdução ao Curso",
+            "tempo": "10:00",
+            "video": "https://www.youtube.com/embed/9XWhNHvGhHU",
           },
           {
-            id: 2,
-            ordem: 2,
-            titulo: "Realizando a Instalação",
-            tempo: "05:00",
-            video: "https://www.youtube.com/embed/9XWhNHvGhHU",
+            "id": 2,
+            "ordem": 2,
+            "titulo": "Realizando a Instalação",
+            "tempo": "05:00",
+            "video": "https://www.youtube.com/embed/9XWhNHvGhHU",
           }
         ]
       },
       
       {
-        id: 2, 
-        titulo: "Curso de JS", 
-        descricao:"Aprenda js na prática", 
-        valor:33.90, 
-        valor_txt:"33,90", 
-        imagem:"https://cdn-images-1.medium.com/max/2000/1*MbVgphFn5dsipV-Sb3eUpg.png",
-        aulas: []
+        "id": 2, 
+        "titulo": "Curso de JS", 
+        "descricao": "Aprenda js na prática", 
+        "valor": 33.90, 
+        "valor_txt": "33,90", 
+        "imagem": "https://cdn-images-1.medium.com/max/2000/1*MbVgphFn5dsipV-Sb3eUpg.png",
+        "aulas": []
       }
     ];
     return lista; // retornando a lista de registros acima.
